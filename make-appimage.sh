f58a23bac526b54ee9bead7b39422c4946a13127
Link4Electronics <link4electronics@gmail.com>
2026-08-15 20:01:15 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,7 +3,7 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q uefitool-ng | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q uefitool-ng | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
@@ -15,8 +15,6 @@ export ALWAYS_SOFTWARE=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/uefitool
-
-# Additional changes can be done in between here
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
