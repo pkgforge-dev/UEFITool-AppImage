98aed14c709a9a3fac844af43150e9a56908e7ea
Link4Electronics <link4electronics@gmail.com>
2026-08-28 18:24:04 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,6 +7,7 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
+     cmake         \
      kvantum       \
      lxqt-qtplugin \
      qt6-base      \
