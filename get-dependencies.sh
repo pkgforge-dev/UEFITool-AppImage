a14b7ebbd6538dea076ab2e3fd3d0c8f26085cc7
Link4Electronics <link4electronics@gmail.com>
2026-08-28 18:12:14 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -9,6 +9,7 @@ echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
      kvantum       \
      lxqt-qtplugin \
+     qt6-base      \
      qt6ct
 
 echo "Installing debloated packages..."
@@ -16,7 +17,7 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package uefitool-ng
+#make-aur-package uefitool-ng
 
 # If the application needs to be manually built that has to be done down here
 
